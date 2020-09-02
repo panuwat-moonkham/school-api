23324ecd85f4af723ad60778d074249c6f299c94
panuwat-moonkham <panuwat.moonkham00@gmail.com>
2020-09-03 01:50:47 +0700

    add destroy and update on enrollmentController

--- app/Controllers/Http/EnrollmentController.js
+++ app/Controllers/Http/EnrollmentController.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const Database = use('Database')
+const Validator = use('Validator')
+const Enrollment = use('App/Models/Enrollment')
 
 function numberTypeParamValidator(number) {
     if(Number.isNaN(parseInt(number))) 
@@ -11,41 +13,46 @@ function numberTypeParamValidator(number) {
 
 class EnrollmentController {
     async index(){
-        const enrollments = await Database.table('enrollments')
+        const {references = undefined} = request.qs
+        
+        const enrollments = Enrollment.query()
+        if(references){
+            const extractedRefences = references.split(",")
+            enrollments.with(extractedRefences)
+        }
 
-        return { status : 200 , error : undefined, data : enrollments}
+        return { status : 200 , error : undefined, data :await enrollments.fetch()}
     }
 
     async show({request}){
         const { id } = request.params
-
-        const validatedValue = numberTypeParamValidator(id)
-
-        if(validatedValue.error) return {status: 500, error : validatedValue.error, data : undefined}
-
-        const enrollment = await Database
-        .select('*')
-        .from('enrollments')
-        .where("enrollment_id",id)
-        .first()
+        const enrollment = await Enrollment.find(id)
 
         return{ status: 200, error : undefined, data : enrollment ||{} }
     }
-
-    async store ({request}){
-        const {mark} = request.body
-
-        const missingKeys=[]
-        if(!mark) missingKeys.push('mark')
-
-        if(missingKeys.length)
-            return {status: 422, error:`${missingKeys} is missing.`, data:undefined}
-
+    async showEnrollment({request}){
+        const {id} = request.params
         const enrollment = await Database
         .table('enrollments')
-        .insert({mark,student_id,subject_id})
+        .where({enrollment_id: id})
+        .innerJoin('students','enrollments.student_id','students.student_id')
+        .first()
 
-        return {status : 200,error : undefined , data : {mark,student_id,subject_id} }
+        return {status: 200, error: undefined, data: enrollment||{}}
+    }
+
+    async store ({request}){
+        const {mark,student_id,subject_id} = request.body
+        const rules ={
+            mark:'required'
+        }
+        const validation = await Validator.validateAll(request.body,rules)
+        if(validation.fails())
+            return {status: 422, error: validation.messages(), data: undefined}
+        
+        const enrollment = await Enrollment.create({mark,student_id,subject_id})
+
+        return {status : 200,error : undefined , data : enrollment }
     }
 
     async update({request}){
@@ -55,7 +62,7 @@ class EnrollmentController {
 
         const enrollmentId = await Database
         .table('enrollments')
-        .where({enrollment_id:id})
+        .where({student_id:id})
         .update({mark,student_id,subject_id})
 
         const enrollment = await Database
